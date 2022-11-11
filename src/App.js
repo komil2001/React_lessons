ae54e555f324a883e7e32b53a5cbd3e74dda70a4
komil00 <abdujabbarovkomil9@gmail.com>
2022-11-12 04:21:04 +0500

    lesson1 increment decrement

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React,{useState} from "react";
 function App() {
+const [count,setCount] = useState(0)
+
+  function inc () {
+    setCount(count+1)
+  }
+  function dec () {
+    setCount(count-1)
+  }
+  function res (){
+    setCount(0)
+  }
   return (
     <>
+    <h3>Count:{count}</h3>
+    <button
+    onClick={inc}
+    >INCR</button>
+    <button
+    onClick={dec}
+    >DECR</button>
+    <button onClick={res}>Reset</button>
     </>
   );
 }
